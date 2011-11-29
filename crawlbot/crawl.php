ac7b6d1c9c5f17cea677620417e71c4c69d0874c
Jordan Perkins <iYorkie@infinityirc.com>
2011-11-29 20:23:01 +0000

    Adding some variables we will be using

--- crawlbot/crawl.php
+++ crawlbot/crawl.php
@@ -3,13 +3,25 @@
 // (Usually '~/crawl/servers') and separate the servers with a new line.
 
 //Initial directory to place crawl data: (Please include a backslash at the end)
+// We wont need this as everything will be stored in mysql! Ill keep it here for now though (J0rd4n)
 $initialDir = "/var/www/crawldata/";
 
 // You should know what to do here... >_>
-$ident = 'CrawlIdent';
-$realname = 'Crawler';
-$nickname = 'Crawler';
-$quitmsg = 'Crawler 3000! - I crawl ur irc netz!';
+$ident = 'IRCReview';
+$realname = 'IRCReview';
+$nickname = 'IRCREVIEW';
+$quitmsg = 'IRCReview Information Collection Bot!';
+// MySQL Details
+$mysqlhost = 'localhost';
+$mysqluser = 'userhere';
+$mysqlpass = 'userpasshere';
+$mysqldatabase = 'databasehere';
+// Some stuff we might not need, but i'll put it in there
+// Call back url in case we add some sort of reaction everytime the crawler activates?
+// Possibly a little message on the site saying how long ago everything was checked
+$callbackurl = 'callbackurlhere.php';
+
+
 
 // End configuration. Do not edit below this line.
 require("./main.php");
